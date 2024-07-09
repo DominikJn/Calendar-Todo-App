416b2a740406435b00f5652b5b649a198fde89fc
Dominik Jon <rajler33@gmail.com>
2024-07-09 14:24:50 +0200

    build: create pages directory and install react-calendar feat: create CalendarPage with Calendar component
    BREAKING CHANGE: create normal CSS file to style this Calendar (I dont even wantto try styling this shit with tailwind)

--- vite-project/src/Layout.tsx
+++ vite-project/src/Layout.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 //components
 import Header from './components/Header'
+//pages
+import CalendarPage from "./pages/calendar/CalendarPage"
 
 
 
@@ -8,6 +10,7 @@ const Layout: React.FC = () => {
     return (
         <div className="bg-bg-color text-main-color h-screen w-screen">
             <Header />
+            <CalendarPage />
         </div>
     )
 }
---
+++ vite-project/src/pages/calendar/CalendarPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+//react-calendar
+import Calendar from 'react-calendar';
+//css
+import './CalendarPage.css'
+
+
+const CalendarPage: React.FC = () => {
+  return (
+    <Calendar />
+  )
+}
+
+export default CalendarPage
