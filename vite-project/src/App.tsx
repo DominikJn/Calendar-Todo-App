d2ebe24e7f2774ac7ba8a4a50990fff899612e26
Dominik Jon <rajler33@gmail.com>
2024-07-10 10:46:27 +0200

    feat: Add Basic Routing in App wrap whole app in BrowserRouter in main.tsx and add routes based on login state in App.tsx

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,16 +1,38 @@
-//react
 import React from "react"
 //layout
 import Layout from "./Layout"
+//react-router
+import { Routes, Route } from "react-router-dom"
+//pages
+import CalendarPage from "./pages/calendar/CalendarPage"
+import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage"
 
 
+
+const userData = {
+  isLogged: false,
+}
 
 const App: React.FC = () => {
-  return (
-    <Layout>
-      
-    </Layout>
-  )
+  return (<>
+      {userData.isLogged
+      ? 
+        <Layout>
+          <Routes>
+            <Route path="/" element={<CalendarPage />} />
+            <Route path="/login" element={<CalendarPage />} />
+            <Route path="/register" element={<CalendarPage />} />
+          </Routes>
+        </Layout>
+      : 
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+    }
+  </>)
 }
 
 export default App
--- vite-project/src/Layout.tsx
+++ vite-project/src/Layout.tsx
@@ -1,16 +1,18 @@
 import React from "react"
 //components
 import Header from './components/Header'
-//pages
-import CalendarPage from "./pages/calendar/CalendarPage"
 
 
 
-const Layout: React.FC = () => {
+interface LayoutProps {
+    children: React.ReactNode
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
         <div className="bg-bg-color text-main-color h-screen w-screen">
             <Header />
-            <CalendarPage />
+            {children}
         </div>
     )
 }
