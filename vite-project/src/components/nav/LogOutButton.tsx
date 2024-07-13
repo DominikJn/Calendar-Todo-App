60ec0afb4d6911b5a5a46c7ebf7abb37dff8dedc
Dominik Jon <rajler33@gmail.com>
2024-07-13 15:47:49 +0200

    refactor: change way userData is being deleted on logout
    Now instead of setting empty object, data is being erased completely,
    also changed number of spaces in className

--- vite-project/src/components/nav/LogOutButton.tsx
+++ vite-project/src/components/nav/LogOutButton.tsx
@@ -1,25 +1,24 @@
-//utils
-import insertDataInLocalStorage from "../../utils/local-storage/insertDataInLocalStorage";
+import React from "react";
 
-function LogoutButton() {
+const LogoutButton: React.FC = () => {
   function handleLogout() {
-    insertDataInLocalStorage("userData", {});
+    localStorage.removeItem("userData");
     location.replace("/login");
   }
 
   return (
     <button
       className="
-                text-xl w-fit py-2 px-4
-                border-solid border-2 border-blue rounded-full
-                hover:bg-blue hover:text-white
-                active:bg-main-blue-active active:text-white
-            "
+        text-xl w-fit py-2 px-4
+        border-solid border-2 border-blue rounded-full
+        hover:bg-blue hover:text-white
+        active:bg-main-blue-active active:text-white
+      "
       onClick={handleLogout}
     >
       Log Out
     </button>
   );
-}
+};
 
 export default LogoutButton;
